d5c23d54d1a0035bf54891604243fc53ca7d280c
mahdihbku <mla.mehdi@hotmail.com>
2020-09-29 00:00:58 +0300

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+gcc -O3 -o param set_param_paillier.c -lcrypto -pthread -lm -lgmp
+gcc -O3 sim.c -lcrypto -pthread -lm
 for n_size in 1024 2048 3072
 do
-	gcc -O3 -o param set_param_paillier.c -lcrypto -pthread -lm -lgmp
 	./param $n_size
 
 	for m in 10 100 200 500 1000
 	do
-		gcc -O3 sim.c -lcrypto -pthread -lm
 		for i in {1..4}
 		do
 			./a.out $m $n_size 4 4
